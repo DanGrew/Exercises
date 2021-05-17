fe808837c7ddba631583e03d5f3edf82030691af
Dan Grew <danielanthonygrew@gmail.com>
2021-05-17 20:08:05 +0100

    PROJECT SETUP:

---
+++ src/main/java/uk/dangrew/exercises/DummyObject.java
@@ -0,0 +1,14 @@
+package uk.dangrew.exercises;
+
+public class DummyObject {
+   
+   private final String name;
+   
+   public DummyObject(String name) {
+      this.name = name;
+   }
+
+   public String getName() {
+      return name;
+   }
+}
